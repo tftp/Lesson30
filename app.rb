da1ab751e24cff7d543540102b0aa4193dd1e45e
tftp <goloborodko@inbox.ru>
2018-11-30 14:21:49 +0300

    Add app.rb and other Begining

---
+++ app.rb
@@ -0,0 +1,16 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+
+set :database, "sqlite3:school.db"
+
+
+get '/' do
+	erb :welcome		
+end
+
+
+
